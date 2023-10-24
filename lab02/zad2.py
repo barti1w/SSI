18c4fbc41f249b60b4d24b9cdf512ad156ec0b0a
Bartosz Wojciechowski <bartiwojciechowski4@gmail.com>
2023-10-24 10:54:16 +0200

    lab03

--- lab02/zad2.py
+++ lab02/zad2.py
@@ -10,26 +10,29 @@ df3 = data.loc[data[4] == 3]
 
 fig, axs = plt.subplots(2, 2, figsize=(10,10))
 
-axs[0,0].scatter(df1[2], df1[3])
-axs[0,0].scatter(df2[2], df2[3])
-axs[0,0].scatter(df3[2], df3[3])
+axs[0,0].scatter(df1[2], df1[3], label = 'Setosa')
+axs[0,0].scatter(df2[2], df2[3], label = 'Versicolour')
+axs[0,0].scatter(df3[2], df3[3], label = 'Virginica')
 axs[0,0].set(xlabel="petal_length_in_cm", ylabel="petal_width_in_cm")
+axs[0,0].legend()
 
-axs[0,1].scatter(df1[1], df1[3])
-axs[0,1].scatter(df2[1], df2[3])
-axs[0,1].scatter(df3[1], df3[3])
+axs[0,1].scatter(df1[1], df1[3], label = 'Setosa')
+axs[0,1].scatter(df2[1], df2[3], label = 'Versicolour')
+axs[0,1].scatter(df3[1], df3[3], label = 'Virginica')
 axs[0,1].set(xlabel="sepal_width_in_cm", ylabel="petal_width_in_cm")
+axs[0,1].legend()
 
-axs[1,0].scatter(df1[0], df1[3])
-axs[1,0].scatter(df2[0], df2[3])
-axs[1,0].scatter(df3[0], df3[3])
+axs[1,0].scatter(df1[0], df1[3], label = 'Setosa')
+axs[1,0].scatter(df2[0], df2[3], label = 'Versicolour')
+axs[1,0].scatter(df3[0], df3[3], label = 'Virginica')
 axs[1,0].set(xlabel="sepal_length_in_cm", ylabel="petal_width_in_cm")
+axs[1,0].legend()
 
-axs[1,1].scatter(df1[1], df1[2])
-axs[1,1].scatter(df2[1], df2[2])
-axs[1,1].scatter(df3[1], df3[2])
+axs[1,1].scatter(df1[1], df1[2], label = 'Setosa')
+axs[1,1].scatter(df2[1], df2[2], label = 'Versicolour')
+axs[1,1].scatter(df3[1], df3[2], label = 'Virginica')
 axs[1,1].set(xlabel="sepal_width_in_cm", ylabel="petal_length_in_cm")
-
+axs[1,1].legend()
 
 
 plt.show()
---
+++ lab03/zad1.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+values = np.loadtxt("spirala.txt", skiprows=0, dtype='double')
+
+m = 3
+iters = 10
+random_indexed = random.sample(range(values.shape[0]), m)
+V = values[random_indexed]
+colors = [[np.round(np.random.rand(), 1) for _ in range(m)] for _ in range(3)]
+
+
+groups = {key: np.array([]).reshape(0, 2) for key in range(m)}
+
+for _ in range(iters):
+    for s in range(len(values)):
+        min_distance = sys.maxsize
+        for middle_point_index in range(len(V)):
+            distance = math.dist(values[s], V[middle_point_index])
+            u = 0
+            if distance < min_distance:
+                min_distance = distance
+                u = middle_point_index
+        groups[u] = np.vstack([groups[u], values[s]])
+
+    for i in range(m):
+        plt.scatter(groups[i][:, 0], groups[i][:, 1], color=colors[i])
+        plt.scatter(V[i][0], V[i][1], color=colors[i], marker='d', edgecolors='black')
+    plt.show()
+
+    for j in range(m):
+        if len(groups[j]) > 0:
+            V[j][0] = np.mean(groups[j][0])
+            V[j][1] = np.mean(groups[j][1])
+    groups = {key: np.array([]).reshape(0, 2) for key in range(m)}
